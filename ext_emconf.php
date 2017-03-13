69a1812cda40aa9ec5f3ac8a30055e594502200d
Christian Futterlieb <christian.futterlieb@agenturamwasser.ch>
2017-03-13 13:24:22 +0100

    [TASK] Add extension description files

---
+++ ext_emconf.php
@@ -0,0 +1,31 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "minipoll".
+ *
+ * Auto generated 13-03-2017 13:18
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Minipoll',
+    'description' => 'A simple, lightweight poll plugin',
+    'category' => 'plugin',
+    'author' => 'Agentur am Wasser | Maeder & Partner AG',
+    'author_email' => 'development@agenturamwasser.ch',
+    'state' => 'alpha',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0-dev',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0-8.999.999'
+        ),
+        'conflicts' => array(),
+        'suggests' => array(
+            'captcha' => ''
+        )
+    )
+);
