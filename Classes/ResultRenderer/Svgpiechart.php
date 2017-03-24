c16d29057fdf1124f29ac8911d0c3c4f9391b00b
Simon Wermuth <simon.wermuth@agenturamwasser.ch>
2017-03-24 15:21:25 +0100

    [TASK] Cleanup svgpiechart renderer

--- Classes/ResultRenderer/Svgpiechart.php
+++ Classes/ResultRenderer/Svgpiechart.php
@@ -28,17 +28,17 @@ class Svgpiechart extends AbstractResultRenderer
      */
     public function getRenderedResults()
     {
-
         return [
-            'paths' => $this->getPaths(),
-            'svgcontent' => 'YAHOOO'
+            'width' => (int) $this->configuration['width'],
+            'height' => (int) $this->configuration['height'],
+            'slices' => $this->getSlices()
         ];
     }
 
     /**
      * @return array
      */
-    protected function getPaths()
+    protected function getSlices()
     {
         // order options
         $orderedOptions = $this->poll->getOptions()->toArray();
@@ -56,36 +56,46 @@ class Svgpiechart extends AbstractResultRenderer
             $orderedOptions = \array_reverse($orderedOptions);
         }
 
+        // Calculate total answer count
+        $totalAnswers = 0;
+        foreach ($orderedOptions as $options) {
+            $totalAnswers += $options->getAnswers()->count();
+        }
+
         $colors = ['#2236e9', '#f40c0c', '#35821f', '#a30cf4', '#daf40c', '#1ecf1e', '#11d6d6', '#fe9800'];
-        $return = [];
-        $previousRadian = 0; // this is 0 because we start at the top of the pie
-        $radius = 190; // radius of the circle
-        $center = new Position(200,200); // center of the circle
-        $seed = new Position(200,10); // seed as postion
-        $lastPosition = new Position(200,10); // same as seed!
+
+        $radius = (int) $this->configuration['pieRadius']; // radius of the circle
+        $centerX = (int) $this->configuration['width'] / 2; // center of the circle
+        $centerY = (int) $this->configuration['height'] / 2;
+
+        $startX = $centerX; // the starting point of the circle
+        $startY = $centerY - $radius;
+
+        // control vars
+        $previousRadianSum = 0.0; // this is 0 because we start at the top of the pie
+        $previousEndX = $startX;
+        $previousEndY = $startY;
 
         // loop through each poll
         $iterator = 0;
+        $return = [];
         foreach ($orderedOptions as $key => $pollOption) {
-            $radian = $this->percentToRadian($this->getPercentage($pollOption));
+            // Get answer count of the current option
+            $answers = $pollOption->getAnswers()->count();
 
-            // get the d tag
-            $slice = $this->getSlice($radian, $previousRadian, $lastPosition, $center, $radius, $seed);
+            // Get percentage of answers of the current option
+            $percent = $this->getPercentage($totalAnswers, $answers);
 
-            // it is current radian + the radian of all previous slices
-            $previousRadian = $radian + $previousRadian;
+            // Get the slice data
+            $slice = $this->getSlice($percent, $radius, $startX, $startY, $centerX, $centerY, $previousEndX, $previousEndY, $previousRadianSum);
 
-            $return[] = [
-                'd' => $slice['d'],
+            // Build the return array
+            $return[] = array_merge($slice, [
                 'fill' => $colors[$iterator],
-                'class' => 'slice'.$key,
-                'votes' => $pollOption->getAnswers()->count(),
-                'text' => [
-                    'text' => $this->getPercentage($pollOption),
-                    'x' => $slice['center']['x'],
-                    'y' => $slice['center']['y']
-                ]
-            ];
+                'id' => $key,
+                'votes' => $answers,
+                'percent' => $percent
+            ]);
 
             $iterator++;
         }
@@ -98,45 +108,52 @@ class Svgpiechart extends AbstractResultRenderer
      *
      * @return array
      */
-    function getSlice($currentRadian, $previousRadian, &$lastPosition, $center, $radius, $seed) {
-        // it is current radian + the radian of all previous slices
-        $radian = $currentRadian + $previousRadian;
+    function getSlice($percent, $radius, $startX, $startY, $centerX, $centerY, &$previousEndX, &$previousEndY, &$previousRadianSum) {
+        // Calculate radians with percentage
+        $radian = $this->percentToRadian($percent);
 
-        // now we get to the point
+        // Calculate the sum of all radian
+        $radianSum = $radian + $previousRadianSum;
+
         // calculate the end point of the current slice
-        $x2 = $seed->getX() + sin($radian) * $radius;
-        $y2 = $seed->getY() + (cos($radian) * -1 + 1) * $radius;
-        $endPos = new Position($x2, $y2);
+        $endX = $startX + sin($radianSum) * $radius;
+        $endY = $startY + (cos($radianSum) * -1 + 1) * $radius;
 
         // calculate the center point of the current slice
-        $textRadius = $radius*0.75;
-        $cX = $seed->getX() + sin($currentRadian/2 + $previousRadian) * $textRadius;
-        $cY = ($seed->getY() + $radius - $textRadius) + (cos($currentRadian/2 + $previousRadian) * -1 + 1) * $textRadius;
+        $textRadius = $radius * 0.75;
+        $cX = $startX + sin($radian/2 + $previousRadianSum) * $textRadius;
+        $cY = ($startY + $radius - $textRadius) + (cos($radian/2 + $previousRadianSum) * -1 + 1) * $textRadius;
 
+        // now create the path string
         // we need something like this:
         // M 50 10 A 40 40 0 0 1 84 69 L 50 50 Z
 
-        // move to the end position of the last pie slice
-        $moveto = new Moveto($lastPosition);
-        // make a arc to the new end position
-        $arcto = new Arcto($radius, $endPos);
-        // draw a line back to the center
-        $lineto = new Lineto($center);
-        // and end the string with a Z to self close the path
-        $end = "Z";
+        // move to the end position of the previous pie slice
+        $path = "M " . $previousEndX . " " . $previousEndY;
 
-        //return 'M 10 10 L 90 10';
-        $path = $moveto->toString() . " " . $arcto->toString() . " " . $lineto->toString() . " " . $end;
+        // make a arc to the new end position
+        $xAxisRotation = 0; // no rotation needed
+        $largeArcFlag = 0; // no largeArcFlag needed
+        $sweepFlag = 1; // we sweep because we go clock wise
+        $path .= "A " . $radius . " " . $radius . " " . $xAxisRotation . " " . $largeArcFlag . " " . $sweepFlag . " " . $endX . " " . $endY;
+
+        // draw a line back to the center
+        $path .= "L " . $centerX . " " . $centerY;
+
+        // and end the string with a Z to self close the path
+        $path .= "Z";
 
         // update the lastPosition for the next slice
-        $lastPosition->setPosition($x2, $y2);
+        $previousEndX = $endX;
+        $previousEndY = $endY;
+
+        // Calculate the sum of previous radian for the next slide
+        $previousRadianSum = $radianSum;
 
         return [
             'd' => $path,
-            'center' => [
-                'x' => $cX,
-                'y' => $cY
-            ]
+            'centerX' => $cX,
+            'centerY' => $cY
         ];
     }
 
@@ -155,140 +172,16 @@ class Svgpiechart extends AbstractResultRenderer
     }
 
     /**
-     * @return int
+     * Gets the percentage of answers compared to total amount of answers
+     *
+     * @return float
      */
-    function getPercentage($pollOption) {
-        // Calculate total answer count
-        $totalAnswers = 0;
-        foreach ($pollOption->getPoll()->getOptions() as $options) {
-            $totalAnswers += $options->getAnswers()->count();
-        }
-        // Get answer count of the current option
-        $answers = $pollOption->getAnswers()->count();
-        $percent = 0;
+    function getPercentage($totalAnswers, $answers) {
+        $percent = 0.0;
         if ($totalAnswers > 0 && $answers > 0) {
             $percent = 100 / $totalAnswers * $answers;
-            if ($roundPercent !== null && $roundPercent >= 0) {
-                $percent = \round($percent, $roundPercent);
-            }
         }
 
         return $percent;
-    }
-}
-
-/**
- * vec2 Position
- *
- * @return void
- */
-class Position {
-    private $x;
-    private $y;
-
-    /**
-     * @return void
-     */
-    public function __construct($x, $y) {
-        $this->x = $x;
-        $this->y = $y;
-    }
-
-    /**
-     * @return string
-     */
-    public function getX() {
-        return $this->x;
-    }
-
-    /**
-     * @return string
-     */
-    public function getY() {
-        return $this->y;
-    }
-
-    /**
-     * @return void
-     */
-    public function setPosition($x, $y) {
-        $this->x = $x;
-        $this->y = $y;
-    }
-}
-
-/**
- * Arcto svg representation
- */
-class Arcto {
-    private $rx;
-    private $ry;
-    private $xAxisRotation;
-    private $largeArcFlag;
-    private $sweepFlag;
-    private $x;
-    private $y;
-
-    /**
-     * @return void
-     */
-    public function __construct($radius, $endPosition) {
-        $this->rx = $radius; // set to radius
-        $this->ry = $radius; // set to radius
-        $this->xAxisRotation = 0; // no rotation needed
-        $this->largeArcFlag = 0; // no largeArcFlag needed
-        $this->sweepFlag = 1; // we sweep because we go clock wise
-        $this->x = $endPosition->getX(); // end position
-        $this->y = $endPosition->getY(); // end position
-    }
-
-    /**
-     * @return string
-     */
-    public function toString() {
-        // A rx ry x-axis-rotation large-arc-flag sweep-flag x y
-        return "A " . $this->rx . " " . $this->ry . " " . $this->xAxisRotation . " " . $this->largeArcFlag . " " . $this->sweepFlag . " " . $this->x . " " . $this->y;
-    }
-}
-
-/**
- * Lineto svg representation
- */
-class Lineto {
-    private $position; // class Position
-
-    /**
-     * @return void
-     */
-    public function __construct($postion) {
-        $this->position = $postion;
-    }
-
-    /**
-     * @return string
-     */
-    public function toString() {
-        return "L " . $this->position->getX() . " " . $this->position->getY();
-    }
-}
-
-/**
- * Moveto svg representation
- */
-class Moveto {
-    private $position; // class Position
-
-    /**
-     * @return void
-     */
-    public function __construct($postion) {
-        $this->position = $postion;
-    }
-
-    /**
-     * @return string
-     */
-    public function toString() {
-        return "M " . $this->position->getX() . " " . $this->position->getY();
     }
 }
