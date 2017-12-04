6c7a132a8dbe422e39380cbcf5c22a3642d984ed
Christian Futterlieb <christian.futterlieb@agenturamwasser.ch>
2017-12-04 12:52:05 +0100

    [TASK] Move default TypoScript to ext_typoscript_setup.txt

--- ext_localconf.php
+++ ext_localconf.php
@@ -23,33 +23,6 @@ defined ('TYPO3_MODE') or die ('Access denied.');
     'Poll' => 'vote'
 ]);
 
-// Add default typoscript setup
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
-/**
- * Default plugin configuration
- */
-plugin.tx_minipoll {
-    view {
-        layoutRootPaths.0 = EXT:minipoll/Resources/Private/Layouts
-        partialRootPaths.0 = EXT:minipoll/Resources/Private/Partials
-        templateRootPaths.0 = EXT:minipoll/Resources/Private/Templates
-    }
-}
-plugin.tx_minipoll.persistence.classes {
-    ' . \AawTeam\Minipoll\Domain\Model\Poll::class . ' {
-        mapping.tableName = tx_minipoll_poll
-    }
-    ' . \AawTeam\Minipoll\Domain\Model\PollOption::class . ' {
-        mapping.tableName = tx_minipoll_poll_option
-    }
-    ' . \AawTeam\Minipoll\Domain\Model\Participation::class . ' {
-        mapping.tableName = tx_minipoll_participation
-    }
-    ' . \AawTeam\Minipoll\Domain\Model\Answer::class . ' {
-        mapping.tableName = tx_minipoll_answer
-    }
-}');
-
 // Add the plugin to new content element wizard
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
 mod.wizards.newContentElement.wizardItems.plugins {
