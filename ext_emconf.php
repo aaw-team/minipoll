a88816d21f0158cc3262aa25ce5aff01b5ceb901
Christian Futterlieb <christian.futterlieb@agenturamwasser.ch>
2021-11-17 15:49:41 +0100

    [TASK] Declare support for TYPO3 v11.5.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.2',
-            'typo3' => '10.4.20-11.4.99',
+            'typo3' => '10.4.20-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [
